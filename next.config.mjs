d15564129a9feeacf968165b98b10369005cda93
contentcreative <chriswoodcockbusiness@gmail.com>
2025-10-05 09:33:45 +0100

    fix YouTube thumbnail image error - add i.ytimg.com to next.config.mjs and fix tsconfig.json

--- next.config.mjs
+++ next.config.mjs
@@ -20,6 +20,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
